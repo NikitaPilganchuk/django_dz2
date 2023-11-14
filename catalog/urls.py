37cf9064b018058ef3ff227144967bdf6ff5eb81
Nikita Pilganchuk <Nikita Pilganchuk>
2023-11-14 20:10:26 +0300

    new changes

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,7 +2,6 @@ from catalog.views import index, contact_information
 from django.urls import path
 
 urlpatterns = [
-    path('', contact_information),
     path('', index),
-
+    path('contacts/', contact_information),
 ]
